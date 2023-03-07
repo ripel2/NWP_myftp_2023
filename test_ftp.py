cdef3c6d190f7f6e76a9d2317fe53ff1b7b9dd29
Lucas Hauszler <lucas.hauszler@epitech.eu>
2023-03-07 12:52:50 +0100

    test: add functionnal tests for QUIT command

--- test_ftp.py
+++ test_ftp.py
@@ -5,13 +5,14 @@ import os
 import sys
 import socket
 
-HOST = '127.0.0.1'
+HOST = "127.0.0.1"
 PORT = 4242
 
-class Test:
-    HOME_PATH = './'
 
-    NAME = 'Test'
+class Test:
+    HOME_PATH = "./"
+
+    NAME = "Test"
 
     def __init__(self):
         self._socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -22,20 +23,20 @@ class Test:
 
     def send(self, data) -> None:
         self._socket.send(data)
-    
+
     def send_command(self, line: str) -> None:
-        self.send(line.encode('utf-8') + b'\r\n')
-    
+        self.send(line.encode("utf-8") + b"\r\n")
+
     def recv(self) -> bytes:
         res = self._socket.recv(16384)
         return res
-    
+
     def parse_code(self, data) -> int:
         try:
             return int(data[:3])
         except ValueError:
             return -1
-    
+
     def run_test(self) -> bool:
         """
         Runs the test.
@@ -45,62 +46,66 @@ class Test:
 
 
 class TestConnection(Test):
-    NAME = 'Test connection'
+    NAME = "Test connection"
 
     def run_test(self) -> bool:
         return True
 
 
 class TestUser(Test):
-    NAME = 'Test USER command'
+    NAME = "Test USER command"
 
     def run_test(self) -> bool:
-        self.send_command('USER Anonymous')
+        self.send_command("USER Anonymous")
         data = self.recv()
         if self.parse_code(data) != 331:
             print("Test failed: USER command did not return 331", file=sys.stderr)
             return False
         return True
-    
+
 
 class TestPass(Test):
-    NAME = 'Test PASS command'
+    NAME = "Test PASS command"
 
     def run_test(self) -> bool:
         if not TestUser.run_test(self):
             return False
 
-        self.send_command('PASS ')
+        self.send_command("PASS ")
         data = self.recv()
         if self.parse_code(data) != 230:
             print("Test failed: PASS command did not return 230", file=sys.stderr)
             return False
 
         return True
-    
+
+
 class TestPwd1(Test):
-    NAME = 'Test PWD basic'
+    NAME = "Test PWD basic"
 
     def run_test(self) -> bool:
         if not TestPass.run_test(self):
             return False
 
-        self.send_command('PWD')
+        self.send_command("PWD")
         data = self.recv()
         if self.parse_code(data) != 257:
             print("Test failed: PWD command did not return 257", file=sys.stderr)
             return False
-        if not "is current directory" in data.decode('utf-8'):
-            print("Test failed: PWD command did not return the correct message", file=sys.stderr)
+        if not "is current directory" in data.decode("utf-8"):
+            print(
+                "Test failed: PWD command did not return the correct message",
+                file=sys.stderr,
+            )
             return False
         return True
-    
+
 
 class TestPwd2(Test):
-    NAME = 'Test PWD while not logged in'
+    NAME = "Test PWD while not logged in"
 
     def run_test(self) -> bool:
-        self.send_command('PWD')
+        self.send_command("PWD")
         data = self.recv()
         code = self.parse_code(data)
         if code == 257:
@@ -113,13 +118,13 @@ class TestPwd2(Test):
 
 
 class TestNOOP(Test):
-    NAME = 'Test NOOP command'
+    NAME = "Test NOOP command"
 
     def run_test(self) -> bool:
         if not TestPass.run_test(self):
             return False
 
-        self.send_command('NOOP')
+        self.send_command("NOOP")
         data = self.recv()
         if self.parse_code(data) != 200:
             print("Test failed: NOOP command did not return 200", file=sys.stderr)
@@ -127,11 +132,12 @@ class TestNOOP(Test):
 
         return True
 
+
 class TestCwd1(Test):
-    NAME = 'Test CWD while not logged in'
+    NAME = "Test CWD while not logged in"
 
     def run_test(self) -> bool:
-        self.send_command('CWD')
+        self.send_command("CWD")
         data = self.recv()
         code = self.parse_code(data)
         if code == 250:
@@ -144,55 +150,58 @@ class TestCwd1(Test):
 
 
 class TestCwd2(Test):
-    NAME = 'Test CWD basic'
+    NAME = "Test CWD basic"
 
     def run_test(self) -> bool:
         if not TestPass.run_test(self):
             return False
 
-        self.send_command('CWD ./tests')
+        self.send_command("CWD ./tests")
         data = self.recv()
         code = self.parse_code(data)
         if code != 250:
             print("Test failed: CWD command did not return 250", file=sys.stderr)
             return False
 
-        self.send_command('PWD')
+        self.send_command("PWD")
         data = self.recv()
         code = self.parse_code(data)
         if code != 257:
             print("Test failed: PWD command did not return 257", file=sys.stderr)
             return False
-        
-        if not "/tests\"" in data.decode('utf-8'):
-            print("Test failed: PWD command did not return the correct path", file=sys.stderr)
+
+        if not '/tests"' in data.decode("utf-8"):
+            print(
+                "Test failed: PWD command did not return the correct path",
+                file=sys.stderr,
+            )
             return False
 
         return True
-    
+
 
 class TestCDUP(Test):
-    NAME = 'Test CDUP command'
+    NAME = "Test CDUP command"
 
     def run_test(self) -> bool:
         if not TestPass.run_test(self):
             return False
 
-        self.send_command('CWD ./tests')
+        self.send_command("CWD ./tests")
         data = self.recv()
         code = self.parse_code(data)
         if code != 250:
             print("Test failed: CWD command did not return 250", file=sys.stderr)
             return False
 
-        self.send_command('CDUP')
+        self.send_command("CDUP")
         data = self.recv()
         code = self.parse_code(data)
         if code != 200:
             print("Test failed: CDUP command did not return 200", file=sys.stderr)
             return False
 
-        self.send_command('PWD')
+        self.send_command("PWD")
         data = self.recv()
         code = self.parse_code(data)
         if code != 257:
@@ -200,6 +209,38 @@ class TestCDUP(Test):
             return False
 
         return True
+
+
+class TestQuit(Test):
+    NAME = "Test QUIT command"
+
+    def run_test(self) -> bool:
+        if not TestPass.run_test(self):
+            return False
+
+        self.send_command("QUIT")
+        data = self.recv()
+        code = self.parse_code(data)
+        if code != 221:
+            print("Test failed: QUIT command did not return 221", file=sys.stderr)
+            return False
+
+        return True
+
+
+class TestQuit2(Test):
+    NAME = "Test QUIT command while not logged in"
+
+    def run_test(self) -> bool:
+        self.send_command("QUIT")
+        data = self.recv()
+        code = self.parse_code(data)
+        if code != 221:
+            print("Test failed: QUIT command did not return 221", file=sys.stderr)
+            return False
+
+        return True
+
 
 TESTS = [
     TestConnection,
@@ -211,14 +252,16 @@ TESTS = [
     TestCwd1,
     TestCwd2,
     TestCDUP,
+    TestQuit,
+    TestQuit2,
 ]
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     if len(sys.argv) != 3:
         print("Usage: python3 test.py <ip> <port>")
         sys.exit(1)
-    
+
     HOST = sys.argv[1]
     PORT = int(sys.argv[2])
 
@@ -229,19 +272,19 @@ if __name__ == '__main__':
         try:
             t = test()
         except Exception as e:
-            print(f'{test.NAME} failed to initialize: {e}')
+            print(f"{test.NAME} failed to initialize: {e}")
             failed += 1
             print()
             continue
         if t.run_test():
-            print(f'{t.NAME} passed')
+            print(f"{t.NAME} passed")
             passed += 1
         else:
-            print(f'{t.NAME} failed')
+            print(f"{t.NAME} failed")
             failed += 1
-        
+
         print()
 
-    print(f'{passed} tests passed, {failed} tests failed')
+    print(f"{passed} tests passed, {failed} tests failed")
     if failed > 0:
         sys.exit(1)
